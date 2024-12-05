bd1d07d023e95ffa503f69a772e7df0b3995603d
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-12-05 14:48:46 +0100

    Add project files.

---
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable SKEXP0050 
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+
+// Build configuration to read from user secrets
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+// Read secrets from configuration
+string yourDeploymentName = configuration["AI:OpenAI:DEPLOYMENT"]!;
+string yourEndpoint = configuration["AI:OpenAI:END_POINT"]!;
+string yourApiKey = configuration["AI:OpenAI:API_KEY"]!;
+
+builder.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourApiKey,
+    yourDeploymentName);
+
+var kernel = builder.Build();
+var result = await kernel.InvokePromptAsync(
+    "Give me a list of breakfast foods with eggs and cheese");
+
+Console.WriteLine(result);
