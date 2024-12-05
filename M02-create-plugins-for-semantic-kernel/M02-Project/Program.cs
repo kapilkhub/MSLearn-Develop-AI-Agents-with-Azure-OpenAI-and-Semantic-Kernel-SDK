b1c2c7d888cc0a916eb66b6f1fefde7e0e5eeeb4
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-12-05 17:39:48 +0100

    travel guide

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -1,6 +1,8 @@
 ﻿#pragma warning disable SKEXP0050 
 using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Plugins.Core;
 
 var builder = Kernel.CreateBuilder();
 
@@ -21,7 +23,28 @@ builder.AddAzureOpenAIChatCompletion(
     yourDeploymentName);
 
 var kernel = builder.Build();
-var result = await kernel.InvokePromptAsync(
-    "Give me a list of breakfast foods with eggs and cheese");
+kernel.ImportPluginFromType<ConversationSummaryPlugin>();
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/TravelPlugins");
 
+
+ChatHistory history = [];
+string input = @"I'm planning an anniversary trip with my spouse. We like hiking, 
+    mountains, and beaches. Our travel budget is $15000";
+
+var result = await kernel.InvokeAsync<string>(prompts["SuggestDestinations"],
+    new() { { "input", input } });
+
+Console.WriteLine(result);
+history.AddUserMessage(input);
+history.AddAssistantMessage(result);
+
+Console.WriteLine("Where would you like to go?");
+input = Console.ReadLine();
+
+result = await kernel.InvokeAsync<string>(prompts["SuggestActivities"],
+    new() {
+        { "history", history },
+        { "destination", input },
+    }
+);
 Console.WriteLine(result);
