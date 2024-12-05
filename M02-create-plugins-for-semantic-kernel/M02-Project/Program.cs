9b2a92b54eafa20fadb270a0f02a76d24b13c8c8
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-12-05 16:32:01 +0100

    commit for music using llm

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -1,6 +1,9 @@
 ﻿#pragma warning disable SKEXP0050 
 using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
+using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System.Text.Json;
 
 var builder = Kernel.CreateBuilder();
 
@@ -21,7 +24,66 @@ builder.AddAzureOpenAIChatCompletion(
     yourDeploymentName);
 
 var kernel = builder.Build();
-var result = await kernel.InvokePromptAsync(
-    "Give me a list of breakfast foods with eggs and cheese");
 
-Console.WriteLine(result);
+var plugins = kernel.CreatePluginFromPromptDirectory("Prompts");
+string input = "G, C";
+
+
+
+var result = await kernel.InvokeAsync(
+    plugins["SuggestChords"],
+    new() { { "startingChords", input } });
+
+// Parse the JSON response
+var jsonDocument = JsonDocument.Parse(result.ToString());
+var suggestedChordsElement = jsonDocument.RootElement.GetProperty("suggestedChords");
+
+var suggestedChords = new Dictionary<string, List<double>>();
+
+foreach (var chord in suggestedChordsElement.EnumerateObject())
+{
+    var chordName = chord.Name;
+    var frequencies = new List<double>();
+
+    foreach (var frequency in chord.Value.EnumerateArray())
+    {
+        frequencies.Add(frequency.GetDouble());
+    }
+
+    suggestedChords[chordName] = frequencies;
+}
+PlayChords(suggestedChords);
+Console.WriteLine("Suggested Chords:");
+
+//Console.WriteLine(string.Join(", ", suggestedChords));
+//PlayChords(["C","C#","D","D#"]);
+
+
+
+static void PlayChords(Dictionary<string, List<double>> suggestedChords)
+{
+    var waveOut = new WaveOutEvent();
+    var mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(44100, 2));
+
+    foreach (var chord in suggestedChords)
+    {
+        foreach (var note in chord.Value)
+        {
+            var sineWave = new SignalGenerator(44100, 2)
+            {
+                Gain = 0.2,
+                Frequency = note,
+                Type = SignalGeneratorType.Sin
+            }.Take(TimeSpan.FromSeconds(10));
+            mixer.AddMixerInput(sineWave);
+        }
+    }
+
+    waveOut.Init(mixer);
+    waveOut.Play();
+    Console.WriteLine("Playing chords...");
+    Console.ReadLine();
+    waveOut.Stop();
+}
+
+
